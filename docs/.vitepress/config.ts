e3d99db952395965b721b51794f6edbad2c2a100
YarKrasavin <101522520+YarKrasavin@users.noreply.github.com>
2025-08-16 19:51:13 +0000

    Changes to be committed: 	new file:   .env 	new file:   .github/workflows/deploy.yml 	new file:   .gitignore 	new file:   .stackblitzrc 	deleted:    README.md 	new file:   docs/.vitepress/config.ts 	new file:   docs/.vitepress/index.md 	new file:   docs/.vitepress/rules.md 	new file:   docs/.vitepress/theme/components/map.vua 	new file:   docs/.vitepress/theme/index.ts 	new file:   docs/.vitepress/theme/style.css 	new file:   docs/about/index.md 	new file:   docs/index.md 	new file:   docs/map.md 	new file:   docs/rules.md 	new file:   docs/wiki/bank.md 	new file:   docs/wiki/boardgames.md 	new file:   docs/wiki/chat.md 	new file:   docs/wiki/cmd.md 	new file:   docs/wiki/donations.md 	new file:   docs/wiki/index.md 	new file:   docs/wiki/lupa.md 	new file:   docs/wiki/play.md 	new file:   docs/wiki/plugins.md 	new file:   package-lock.json 	new file:   package.json

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,101 @@
+import { defineConfig } from 'vitepress';
+
+// refer https://vitepress.dev/reference/site-config for details
+export default defineConfig({
+  lang: 'ru-RU',
+  cleanUrls: true,
+  title: 'Milandur',
+  description: 'Milandur - Minecraft Сервер',
+  head: [['link', { rel: 'icon', href: '/logo-64.png' }]],
+
+  themeConfig: {
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          root: {
+            translations: {
+              button: {
+                buttonText: 'Поиск',
+                buttonAriaLabel: 'Поиск',
+              },
+              modal: {
+                displayDetails: 'Отобразить подробный список',
+                resetButtonTitle: 'Сбросить поиск',
+                backButtonTitle: 'Закрыть поиск',
+                noResultsText: 'Нет результатов по запросу',
+                footer: {
+                  selectText: 'выбрать',
+                  selectKeyAriaLabel: 'выбрать',
+                  navigateText: 'перейти',
+                  navigateUpKeyAriaLabel: 'стрелка вверх',
+                  navigateDownKeyAriaLabel: 'стрелка вниз',
+                  closeText: 'закрыть',
+                  closeKeyAriaLabel: 'esc',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    logo: '/logo.png',
+    nav: [
+      { text: 'Главное', link: '/' },
+      // { text: 'О сервере', link: '/about' },
+      { text: 'Правила', link: '/rules' },
+      { text: 'Вики', link: '/wiki' },
+
+      // {
+      //   text: 'Dropdown Menu',
+      //   items: [
+      //     { text: 'Item A', link: '/item-1' },
+      //     { text: 'Item B', link: '/item-2' },
+      //     { text: 'Item C', link: '/item-3' },
+      //   ],
+      // },
+
+      // ...
+    ],
+    socialLinks: [{ icon: 'discord', link: 'https://discord.gg/B3aXZ77QPe' }],
+
+    outline: {
+            label: "На этой странице",
+        },
+    docFooter: {
+            prev: 'Предыдущая страница',
+            next: 'Следующая страница'
+        },
+    sidebar: {
+      // Эта боковая панель отображается, когда пользователь находится в директории `guide`
+      '/about/': [
+        {
+          text: 'О сервере',
+          items: [{ text: 'О Milandur', link: '/about/' }],
+        },
+      ],
+      '/wiki/': [
+        {
+          text: 'Руководство',
+          items: [
+            { text: 'Начало', link: '/wiki/' },
+            { text: 'Как играть?', link: '/wiki/play' }
+          ],
+        },
+        {
+          text: 'Механики и команды',
+          collapsed: true,
+          items: [
+            { text: 'Чат', link: '/wiki/chat' },
+            { text: 'Банк', link: '/wiki/bank' },
+            { text: 'Лупа', link: '/wiki/lupa' },
+            { text: 'Настольные игры', link: '/wiki/boardgames' },
+            { text: 'Команды', link: '/wiki/cmd' },
+            { text: 'Мелкие плагины', link: '/wiki/plugins' },
+            { text: 'Пожертвования', link: '/wiki/donations' },
+          ],
+        },
+      ],
+    },
+  },
+});
